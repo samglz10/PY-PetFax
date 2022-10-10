8b56806c90372e531c5e984979438186b706dfec
Samuel Gonzalez <samglz10@gmail.com>
2022-10-09 22:10:45 -0400

    css styling added

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -2,9 +2,16 @@ from flask import Flask
 
 def create_app(): 
     app = Flask(__name__)
+
+    # factory 
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost:5432/petfax-app_db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
     
     from . import pet
     app.register_blueprint(pet.bp)
+
+    from . import fact
+    app.register_blueprint(fact.bp)
 
     @app.route('/')
     def hello(): 
@@ -16,3 +23,7 @@ def create_app():
 
     return app
 
+
+# factory 
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
--- petfax/fact.py
+++ petfax/fact.py
@@ -1,7 +1,16 @@
-from flask import ( Blueprint, render_template ) 
+from flask import ( Blueprint, render_template, request, redirect) 
 
-bp = Blueprint('fact', __name__, url_prefix="/facts")
+bp = Blueprint('fact', __name__, url_prefix="/facts", template_folder='templates')
+
+
+@bp.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method =='POST':
+        print(request.form)
+        return redirect('/facts')
+
+    return render_template('facts/index.html')
 
 @bp.route('/new')
 def fact(): 
-    return render_template('new.html')
+    return render_template('facts/new.html')
